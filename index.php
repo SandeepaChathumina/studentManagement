b4a3e55e67dc106394364204bfd11be8db848ee2
Sandeepa-Karunanayake <sandeepachathuminaofficial@gmail.com>
2025-06-27 16:16:29 +0530

    updated

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
     <title>Student Management System</title>
 </head>
 <body>
-    <h1>This is the first tutorial</h1>
+    <!--<h1>This is the first tutorial</h1>-->
+    <nav>
+        <label>Smart School</label>
+        <ul>
+            <li><a href="sample.php">Home</a></li>
+            <li><a href="student.php">Contact</a></li>
+            <li><a href="teacher.php">Admission</a></li>
+            <li><a href="class.php">Login</a></li>
+    </nav>
 </body>
 </html>
