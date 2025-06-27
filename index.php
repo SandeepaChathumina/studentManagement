2f181c3c0eff87eb72072d972a4ec8c06079b79b
Sandeepa-Karunanayake <sandeepachathuminaofficial@gmail.com>
2025-06-27 16:46:38 +0530

    updated

--- index.php
+++ index.php
@@ -4,16 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Management System</title>
+    <link rel="stylesheet" href="style.css">
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <!--<h1>This is the first tutorial</h1>-->
     <nav>
-        <label>Smart School</label>
+        <label class="logo">Smart School</label>
         <ul>
             <li><a href="sample.php">Home</a></li>
             <li><a href="student.php">Contact</a></li>
             <li><a href="teacher.php">Admission</a></li>
-            <li><a href="class.php">Login</a></li>
+            <li><a href="class.php" class="btn btn-success">Login</a></li>
     </nav>
 </body>
 </html>
