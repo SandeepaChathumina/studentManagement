2248b694c3af218316cfa7bd45dd5a9e96747141
Sandeepa-Karunanayake <sandeepachathuminaofficial@gmail.com>
2025-06-28 14:20:14 +0530

    updated

--- adminhome.php
+++ adminhome.php
@@ -18,10 +18,42 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="admin.css">
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </head>
 <body>
-    <h1>Admin Dashboard</h1>
+    <header class="header">
+        <a href="">Admin Dashboard</a>
 
-    <a href="logout.php">Logout</a>
+        <div class="logout">
+            <a href="logout.php" class="btn btn-primary">Logout</a>
+        </div>
+    </header>
+
+    <aside>
+        <ul>
+            <li><a href="">Admission</a></li>
+            <li><a href="">Add Student</a></li>
+            <li><a href="">View Student</a></li>
+            <li><a href="">Add Teacher</a></li>
+            <li><a href="">View Teacher</a></li>
+            <li><a href="">Add Courses</a></li>
+            <li><a href="">View Courses</a></li>
+        </ul>
+    </aside>
+
+    <div class="content">
+        <h1>Welcome to Admin Dashboard</h1>
+        <p>This is the admin dashboard where you can manage students, teachers, and courses.</p>
+    </div>
 </body>
 </html>
--- studenthome.php
+++ studenthome.php
@@ -18,10 +18,38 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="admin.css">
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </head>
 <body>
-    <h1>Student Dashboard</h1>
+    <header class="header">
+        <a href="">Student Dashboard</a>
 
-    <a href="logout.php">Logout</a>
+        <div class="logout">
+            <a href="logout.php" class="btn btn-primary">Logout</a>
+        </div>
+    </header>
+
+    <aside>
+        <ul>
+            <li><a href="">My Courses</a></li>
+            <li><a href="">My Results</a></li>
+            
+        </ul>
+    </aside>
+
+    <div class="content">
+        <h1>Welcome to Admin Dashboard</h1>
+        <p>This is the admin dashboard where you can manage students, teachers, and courses.</p>
+    </div>
 </body>
 </html>
